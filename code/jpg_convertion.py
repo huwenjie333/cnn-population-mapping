8158f28a31e797d113658bc65a4e8f9e76ad647b
huwenjie333 <wenjie.hu2013@gmail.com>
2018-05-16 06:52:49 +0000

    nan bug fix in df.l8_path

--- code/jpg_convertion.py
+++ code/jpg_convertion.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import re,os,time, math, gdal
+from numba import jit
 from PIL import Image
 
 df = pd.read_csv('/home/timhu/data/all_jpgpaths_clean_538k_May14.csv')
@@ -12,7 +13,7 @@ Wl8 = Hl8 = 150 # Landsat 30m resolution per pixel
 Ws1 = Hs1 = 450 # Sentinel-1 10m resolution per pixel
 
 
-
+@jit
 def load_tif_from_file(file, crop_width, crop_height):
     img_file = gdal.Open(file)
     width, height = img_file.RasterXSize, img_file.RasterYSize
@@ -26,6 +27,7 @@ def load_tif_from_file(file, crop_width, crop_height):
     
     return img_data
 
+@jit
 def jpg_convert_save(tif_path, crop_width, crop_height):
     jpg_path = re.sub('tif', 'jpg', tif_path)
     jpg_path = re.sub('500x500', str(crop_width)+'x'+str(crop_height), jpg_path)
@@ -35,14 +37,16 @@ def jpg_convert_save(tif_path, crop_width, crop_height):
     
     im.save(os.path.join(JPG_DIR, jpg_path), format='JPEG')
     
-
+duration = 0
 for i in range(len(df)):
+    start_time = time.time() # record the time used for each batch 
     if i % 100 == 0:
-        print('finish:', i)
+        print('finish: %d with %.2f sec each iteration' % (i, duration))
     l8_path = os.path.join(TIF_DIR, df.l8_vis_path[i])
     s1_path = os.path.join(TIF_DIR, df.s1_vis_path[i])
     
     jpg_convert_save(l8_path, Wl8, Hl8)
     jpg_convert_save(s1_path, Ws1, Hs1)
+    duration = time.time() - start_time
     
     
